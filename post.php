3c0376594a597a01cd4b7775f0a7c2e883130f9e
Rémy <remy.guillet44@gmail.com>
2020-12-14 17:25:37 +0100

    update : post

--- post.php
+++ post.php
@@ -1,22 +1,42 @@
 <?php include_once 'includes/header.php'; ?>
-<?php include_once 'includes/navbar.php' ;?>
+<?php include_once 'includes/navbar.php'; ?>
 
 <main>
     <section class="container">
-        <div>
-            <h1 class="pt-3 d-flex justify-content-center">Publication</h1>
-        </div>
-        <div>
-            <form method="post" action="#" class="my-5">
-                <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Votre publication</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="5" name="post"></textarea>
+
+        <div class="container">
+
+            <div>
+                <h1 class="pt-3 d-flex justify-content-center">Publication</h1>
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <form method="post" action="#" class="my-5">
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea1">Votre publication</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"
+                                      name="post"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-warning mt-3"> Publier</button>
+                    </form>
+                </div>
+                <div class="col">
+                    <h3 class="mt-3">Vos dernières publications</h3>
+                    <div class="card mt-4" style="width: 40rem;">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Cras justo odio</li>
+                            <li class="list-group-item">Dapibus ac facilisis in</li>
+                            <li class="list-group-item">Vestibulum at eros</li>
+                        </ul>
+                    </div>
+
                 </div>
 
-                <button type="submit" class="btn btn-warning mt-3"> Publier </button>
-            </form>
-
+            </div>
         </div>
+
     </section>
 </main>
 
