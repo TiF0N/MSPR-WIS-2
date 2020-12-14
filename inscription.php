f614227d6aebcd08fedf98abc0530bebd7798535
Rémy <remy.guillet44@gmail.com>
2020-12-14 15:55:59 +0100

    update : inscrption

--- inscription.php
+++ inscription.php
@@ -27,13 +27,11 @@
                     <label for="password" class="form-label">Mot de passe</label>
                     <input type="password" class="form-control" id="password" name="password">
                 </div>
-<<<<<<< Updated upstream
-=======
 
                 <div class="mb-5">
                     <a href="login.php">Se connecter</a>
                 </div>
->>>>>>> Stashed changes
+
                 <div class="mb-3 form-check">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1">
                     <label class="form-check-label" for="exampleCheck1" >Accepter les conditions</label>
