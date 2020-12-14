1fa6fef6ff6fe919edaf4d1d354daeb11f31a661
hasinarnd <hasinarandriamora97@gmail.com>
2020-12-14 16:20:37 +0100

    profile php

--- profile.php
+++ profile.php
@@ -1,3 +1,40 @@
 <?php include_once 'includes/header.php'; ?>
+<?php include_once 'includes/navbar.php'; ?>
+    <main id="main">
+        <section class="section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 d-flex justify-content-center align-items-center flex-column py-3">
+                        <img class="rounded-circle" src="https://via.placeholder.com/100x100/" alt="">
+                        <h1 class="h2 mt-4">Bonjour, Name !</h1>
+                        <a class="btn btn-primary" href="#">Modifier mon profil</a>
+                    </div>
+                    <div class="col-lg-8 py-5">
+                        <div class="card">
+                            <div class="card-body">
+                                <h2 class="h3">My Biographie</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias asperiores
+                                    aut beatae debitis, earum enim et ex fuga, hic labore non sapiente voluptatum. A
+                                    earum fugit in obcaecati vero. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias asperiores
+                                    aut beatae debitis, earum enim et ex fuga, hic labore non sapiente voluptatum. A
+                                    earum fugit in obcaecati vero.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section">
+            <div class="container py-4">
+                <h2 class="h2">Ce que vous avez aimé</h2>
+                <div class="card">
+                    <div class="card-body">
+                        <p>La publication...</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
 
 <?php include_once 'includes/footer.php'; ?>
