f2d17112b1bcd2ade42dc1865cfe1787026304c7
Mart1 <martinlb28@gmail.com>
2020-12-14 16:47:15 +0100

    navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,33 +1,39 @@
 <header>
-    <section id="navbar">
+    <section id="navbar d-flex justify-content-between">
         <nav class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">SymphoLink</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            <div class="container-fluid  ">
+
+
+                    <a class="navbar-brand" href="#">SymphoLink</a>
+
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+
+
+
+                <input class="form-control form-control-dark  w-100" type="text" placeholder="Search" aria-label="Search">
+
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#"></a>
-                        </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                               data-toggle="dropdown" aria-expanded="false">
                                 Mon Prénom Nom
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="profile.php">Profile</a></li>
                                 <li><a class="dropdown-item" href="#">Paramètres</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="#">Déconnexion</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item text-danger" href="#">Déconnexion</a></li>
                             </ul>
                         </li>
-
                     </ul>
-                    <form class="d-flex">
-                        <input class="form-control mr-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-warning" type="submit">Search</button>
-                    </form>
                 </div>
             </div>
         </nav>
