b7dd2ff947bb81650eb3d489fdc884bb2215251f
hasinarnd <hasinarandriamora97@gmail.com>
2020-12-14 17:07:52 +0100

    favicon

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/png" href="inc/logo_simple.png" />
     <link rel="stylesheet" href="assets/css/app.css">
     <title>SymphoLink | Listen to your network</title>
 </head>
