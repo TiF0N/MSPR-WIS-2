d0e3f3612206fbc381ecd2bfb88af1d44f577c38
Rémy <remy.guillet44@gmail.com>
2020-12-14 11:13:25 +0100

    update : title name

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!doctype html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="assets/css/app.css">
-    <title>TheNetwork</title>
+    <title>SymphoLink | Listen to your network</title>
 </head>
 <body>
 
