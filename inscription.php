97b60c20bcf53555fbbcf9bc80a1bf757a847300
Rémy <remy.guillet44@gmail.com>
2020-12-14 15:55:02 +0100

    Auto stash before merge of "master" and "origin/master"

--- inscription.php
+++ inscription.php
@@ -8,34 +8,37 @@
             <form class="my-5">
                 <div class="mb-3">
                     <label for="name" class="name">Nom</label>
-                    <input type="text" class="form-control" id="name" required>
+                    <input type="text" class="form-control" id="name" name="name" required>
                 </div>
                 <div class="mb-3">
                     <label for="first_name" class="first_name">Prénom</label>
-                    <input type="text" class="form-control" id="first_name" required>
+                    <input type="text" class="form-control" id="first_name" name="first_name" required>
                 </div>
                 <div class="mb-3">
                     <label for="dateofbirth" class="dateofbirth">Date de naissance</label>
-                    <input type="date" class="form-control" id="dateofbirth" required>
+                    <input type="date" class="form-control" id="dateofbirth" name="dateofbirth" required>
                 </div>
                 <div class="mb-3">
-                    <label for="Email" class="form-label">Adresse mail</label>
-                    <input type="email" class="form-control" id="Email" aria-describedby="emailHelp">
-                    <div id="Email" class="form-text">Nous ne partagerons pas votre mail avec qui que ce soit</div>
+                    <label for="email" class="form-label">Adresse mail</label>
+                    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
+                    <div id="Email" class="form-text">Votre mail est protégé </div>
                 </div>
                 <div class="mb-3">
                     <label for="password" class="form-label">Mot de passe</label>
-                    <input type="password" class="form-control" id="password">
+                    <input type="password" class="form-control" id="password" name="password">
                 </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Confirmer son mot de passe</label>
-                    <input type="password" class="form-control" id="password">
+<<<<<<< Updated upstream
+=======
+
+                <div class="mb-5">
+                    <a href="login.php">Se connecter</a>
                 </div>
+>>>>>>> Stashed changes
                 <div class="mb-3 form-check">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Accepter les conditions</label>
+                    <label class="form-check-label" for="exampleCheck1" >Accepter les conditions</label>
                 </div>
-                <button type="button" class="btn btn-primary">Envoyer</button>
+                <button type="button" class="btn btn-warning">Envoyer</button>
             </form>
         </section>
 
--- login.php
+++ login.php
@@ -1,3 +1,5 @@
-<?php include_once 'includes/header.php'; ?>
+<?php include_once 'includes/header.php';?>
+
+<button class="btn btn-primary"> Bonjour </button>
 
 <?php include_once 'includes/footer.php'; ?>
