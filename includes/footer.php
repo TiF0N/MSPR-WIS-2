cac69a758d8ca9a713088490dfe4d6e43505b0db
Rémy <remy.guillet44@gmail.com>
2020-12-14 12:58:35 +0100

    update : footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,78 +1,57 @@
 
 <!-- Footer -->
-<footer class="page-footer font-small blue pt-4 bg-light">
 
-    <!-- Footer Links -->
+
+<footer class="page-footer font-small blue pt-4 bg-dark">
     <div class="container-fluid text-center text-md-left">
 
-        <!-- Grid row -->
+
         <div class="row">
 
-            <!-- Grid column -->
             <div class="col-md-6 mt-md-0 mt-3">
 
-                <!-- Content -->
-                <h5 class="text-uppercase">SymphoLink</h5>
-                <p>Le plus grand site de partage musicale</p>
+                <h5 class="text-uppercase text-white" >SymphoLink</h5>
+                <p class="text-white">Le plus grand site de partage musicale</p>
 
             </div>
-            <!-- Grid column -->
 
             <hr class="clearfix w-100 d-md-none pb-3">
 
-            <!-- Grid column -->
             <div class="col-md-3 mb-md-0 mb-3">
 
-                <!-- Links -->
-                <h5 class="text-uppercase">Links</h5>
+                <h5 class="text-uppercase text-white">Links</h5>
 
                 <ul class="list-unstyled">
                     <li>
-                        <a href="#!">Link 1</a>
+                        <a class="text-warning" href="#!">Link 1</a>
                     </li>
                     <li>
-                        <a href="#!">Link 2</a>
+                        <a class="text-warning" href="#!">Link 2</a>
                     </li>
-                    <li>
-                        <a href="#!">Link 3</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 4</a>
-                    </li>
+
                 </ul>
 
             </div>
-            <!-- Grid column -->
 
-            <!-- Grid column -->
             <div class="col-md-3 mb-md-0 mb-3">
 
-                <!-- Links -->
-                <h5 class="text-uppercase">Links</h5>
+                <h5 class="text-uppercase text-white">Links</h5>
 
-                <ul class="list-unstyled">
+                <ul class="list-unstyled ">
                     <li>
-                        <a href="#!">Link 1</a>
+                        <a class="text-warning" href="#!">Link 1</a>
                     </li>
                     <li>
-                        <a href="#!">Link 2</a>
+                        <a class="text-warning" href="#!" >Link 2</a>
                     </li>
-                    <li>
-                        <a href="#!">Link 3</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 4</a>
-                    </li>
+
                 </ul>
 
             </div>
-            <!-- Grid column -->
 
         </div>
-        <!-- Grid row -->
 
     </div>
-    <!-- Footer Links -->
 
     <!-- Copyright -->
     <div class="footer-copyright text-center py-3 bg-dark text-white">© 2020 Copyright
--- welcome-page.php
+++ welcome-page.php
@@ -1,8 +1,10 @@
 <?php include_once 'includes/header.php'; ?>
 
+
 <main id="main" style="background-color: black;">
     <section class="container-fluid" style="background-image: url(inc/logo_simple.png); background-position: center; background-repeat: no-repeat;">
         <div class="welcome d-flex justify-content-center m-5">
+
             <ul class="list-group m-5">
                 <li class="list-group-item list-group-item-action">
                     <a href="inscription.php" class="btn">Inscription</a>
