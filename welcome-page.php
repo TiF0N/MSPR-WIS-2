5a5c4efbaf4b9c0c00a6685ae6e0d36472fe220d
hasinarnd <hasinarandriamora97@gmail.com>
2020-12-14 12:56:00 +0100

    welcome-page/inscription

--- welcome-page.php
+++ welcome-page.php
@@ -1,8 +1,8 @@
 <?php include_once 'includes/header.php'; ?>
 
-<main id="main" style="background-color: black">
-    <section class="container-fluid" style="background-image: url(inc/logo-transparent.png); background-position: center; background-repeat: no-repeat; background-attachment: fixed; padding: 50px">
-        <div class="welcome d-flex justify-content-center my-5">
+<main id="main" style="background-color: black;">
+    <section class="container-fluid" style="background-image: url(inc/logo_simple.png); background-position: center; background-repeat: no-repeat;">
+        <div class="welcome d-flex justify-content-center m-5">
             <ul class="list-group m-5">
                 <li class="list-group-item list-group-item-action">
                     <a href="inscription.php" class="btn">Inscription</a>
@@ -12,10 +12,7 @@
                 </li>
             </ul>
         </div>
-
     </section>
 </main>
-
-
 
 <?php include_once 'includes/footer.php'; ?>
