3fcf7fefbfd78392febe197fbf813d7bc3331b6c
hasinarnd <hasinarandriamora97@gmail.com>
2020-12-15 14:03:58 +0100

    hasina : navbar dropdown

--- includes/components/navbar.php
+++ includes/components/navbar.php
@@ -2,20 +2,22 @@
     <section id="navbar d-flex justify-content-between">
         <nav class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark">
             <div class="container-fluid">
-                    <a class="navbar-brand" href="#">SymphoLink</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                <input class="form-control form-control-dark w-100" type="text" placeholder="Rechercher" aria-label="Search">
+                <a class="navbar-brand" href="#">SymphoLink</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <input class="form-control form-control-dark w-100" type="text" placeholder="Rechercher"
+                       aria-label="Search">
                 <button class="btn btn-warning ms-2" type="button">Rechercher</button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                                data-bs-toggle="dropdown" aria-expanded="false">
-                                Mon Prénom Nom
+                                <img class="rounded-circle m-1" src="https://via.placeholder.com/50x50" alt=""> Prénom
+                                Nom
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="../../profile.php">Profile</a></li>
