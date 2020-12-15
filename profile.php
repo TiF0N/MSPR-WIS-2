04aa991da0e37c191b1774e78d584e9d1b6ada60
Rémy <remy.guillet44@gmail.com>
2020-12-15 20:15:13 +0100

    update : profil

--- profile.php
+++ profile.php
@@ -1,19 +1,24 @@
 <?php include_once 'includes/layout/header.php'; ?>
 <?php include_once 'includes/components/navbar.php'; ?>
+
+<?php $user = getUser($_SESSION['id']); ?>
     <main id="main">
         <section class="section">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-4 d-flex justify-content-center align-items-center flex-column py-3">
                         <img class="rounded-circle" src="https://via.placeholder.com/100x100/" alt="">
-                        <h1 class="h2 mt-4">Bonjour <?php echo $_SESSION['nom'] . ' '. $_SESSION['firstname'];?></h1>
-                        <a class="btn btn-primary" href="edit-user.php">Modifier mon profil</a>
+                        <h1 class="h2 mt-4">Profil de : <?php echo $user['nom'] . ' ' . $user['firstname']; ?></h1>
+                        <a class="btn btn-warning" href="edit-user.php">Modifier mon profil</a>
                     </div>
                     <div class="col-lg-8 py-5">
+
                         <div class="card">
+                            <div class="card-header">
+                                <h2 class="text-warning ">BIO</h2>
+                            </div>
                             <div class="card-body">
-                                <h2 class="h3">BIO</h2>
-                                <p><?php $_SESSION['bio']; ?></p>
+                                <p class="card-text"><?php echo $user['bio']; ?></p>
                             </div>
                         </div>
                     </div>
@@ -22,7 +27,7 @@
         </section>
         <section class="section">
             <div class="container py-4">
-                <h2 class="h2">Ce que vous avez aimé</h2>
+                <h2 class="h2">Publications likés :</h2>
                 <div class="card">
                     <div class="card-body">
                         <p>La publication...</p>
