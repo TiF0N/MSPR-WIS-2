f2050ee14dba78467b4d6f90948f3815c6cc2f7c
hasinarnd <hasinarandriamora97@gmail.com>
2020-12-16 09:52:55 +0100

    post update

--- post.php
+++ post.php
@@ -1,7 +1,9 @@
 <?php include_once 'includes/layout/header.php'; ?>
 <?php include_once 'includes/components/navbar.php'; ?>
 
-<?php $posts = getPosts(); ?>
+<?php $posts = getPosts();
+
+?>
 
 <main>
     <section class="container">
@@ -16,8 +18,9 @@
                 <div class="col">
                     <form method="post" action="./includes/store-post.php" class="my-5">
                         <div class="form-group">
-                            <label for="exampleFormControlTextarea1">Votre publication</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
+                            <label class="h5" for="exampleFormControlTextarea1">Votre publication</label>
+                            <textarea placeholder="Ecrire votre publication..." class="form-control"
+                                      id="exampleFormControlTextarea1" rows="5"
                                       name="content"></textarea>
                         </div>
                         <button type="submit" class="btn btn-warning mt-3">Publier</button>
@@ -25,16 +28,16 @@
                     </form>
                 </div>
                 <div class="col">
-                    <h3 class="mt-3">Vos dernières publications</h3>
+                    <h3 class="d-flex justify-content-center">Vos dernières publications</h3>
                     <?php if ($posts = getPosts()): ?>
-                        <div class=" card" style="max-width: 50rem;">
-                            <?php foreach ($posts as $post): ?>
-                                <span class="list-group-item list-group-item-action bg-light ">
-                                    <?php echo $posts['content']; ?>
-                                    <a" class="btn btn-outline-primary ml-5">Supprimer</a>
-                                </span>
-                            <?php endforeach; ?>
-                        </div>
+                            <ul class="mt-4 p-2 rounded-2">
+                                <?php foreach ($posts as $post): ?>
+                                    <li class="list-group-item list-group-item-action bg-dark text-white p-3 regular shadow">
+                                        <?php echo $posts['content']; ?>
+                                        <a class="btn btn-danger ms-5">Supprimer</a>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
                     <?php endif; ?>
 
                 </div>
